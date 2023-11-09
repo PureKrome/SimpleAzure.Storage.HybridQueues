f12b810b16fa947e47303c7951f4fa522451b647
David Wengier <git@wengier.com>
2023-11-09 21:43:36 +1100

    Remove extra logging message
    The BeginScope method is on ILogger, not ILogger<T>, so there was no difference between this and the method below

--- src/SimpleAzure.Storage.HybridQueue/LoggingExtensions.cs
+++ src/SimpleAzure.Storage.HybridQueue/LoggingExtensions.cs
@@ -4,20 +4,6 @@ namespace WorldDomination.SimpleAzure.Storage.HybridQueues;
 
 public static class LoggerExtensions
 {
-    public static IDisposable? BeginCustomScope<T>(this ILogger<T> logger, params (string Name, object? Value)[] scopeItems)
-    {
-        ArgumentNullException.ThrowIfNull(scopeItems);
-
-        var scopeProps = new Dictionary<string, object?>();
-
-        foreach (var (name, value) in scopeItems)
-        {
-            scopeProps[name] = value;
-        }
-
-        return logger.BeginScope(scopeProps);
-    }
-
     public static IDisposable? BeginCustomScope(this ILogger logger, params (string Name, object? Value)[] scopeItems)
     {
         ArgumentNullException.ThrowIfNull(scopeItems);
